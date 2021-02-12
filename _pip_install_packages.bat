9d63baa6ff023de238a2cd426c2b951e34ad358d
Picuino <25557416+picuino@users.noreply.github.com>
2021-02-12 20:44:59 +0100

    Update _pip_install_packages.bat

--- _pip_install_packages.bat
+++ _pip_install_packages.bat
@@ -1,9 +1,10 @@
-set python=venv\Scripts\python.exe
+set PATH=venv\Scripts;%PATH%
+call activate.bat
 
-%python% -m pip install --upgrade pip
-%python% -m pip install --upgrade PyYAML
-%python% -m pip install --upgrade Jinja2
-%python% -m pip install --upgrade Pillow
-%python% -m pip install --upgrade python-docx
+python -m pip install --upgrade pip
+python -m pip install --upgrade PyYAML
+python -m pip install --upgrade Jinja2
+python -m pip install --upgrade Pillow
+python -m pip install --upgrade python-docx
 
 pause
